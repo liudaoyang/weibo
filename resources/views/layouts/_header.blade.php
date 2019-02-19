70df0fb185c13236db50eee49655323c60ac7113
bigliu <www.teacheryou.cn@gmail.com>
2019-02-19 20:14:27 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="/" class="navbar-brand">Weibo App</a>
+        <a href="{{route('home')}}" class="navbar-brand">Weibo App</a>
         <ul class="navbar-nav justfy-content-end">
             <li class="nav-item">
-                <a href="/help" class="navbar-link">帮助</a>
+                <a href="{{route('help')}}" class="navbar-link">帮助</a>
 
             </li>
-            <li class="nav-item"><a href="/#" class="navbar-link">登录</a></li>
+            <li class="nav-item"><a href="#" class="navbar-link">登录</a></li>
         </ul>
     </div>
 </nav>
